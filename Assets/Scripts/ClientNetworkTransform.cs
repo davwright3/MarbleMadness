44f9b84975b96889ac9f4f3d5aebb5ab046014ed
davwright3 <dwright3@ualr.edu>
2024-10-05 18:47:54 -0500

    Update 5 Oct late

---
+++ Assets/Scripts/ClientNetworkTransform.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Netcode.Components;
+
+[DisallowMultipleComponent]
+public class ClientNetworkTransform : NetworkTransform
+{
+    // Start is called before the first frame update
+    protected override bool OnIsServerAuthoritative()
+    { return false; }
+}
